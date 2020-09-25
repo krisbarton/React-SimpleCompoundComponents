9e1d909004605cd04fc0da04741823d7ee7bb754
Kris Barton <krisbarton@gmail.com>
2020-09-25 14:46:15 +0100

    Create index.js

---
+++ src/components/Header/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Header = () => {
+
+    return (
+        <header>
+            <a href="https://www.thatcocktail.com">
+                <img
+                    src="https://www.thatcocktail.com/wp-content/themes/thatcocktail/assets/thatcocktail200.png"
+                    alt="ThatCocktail.com - Cocktails made easy!"
+                />
+            </a>
+        </header>
+    )
+
+}
+
+export default Header;
